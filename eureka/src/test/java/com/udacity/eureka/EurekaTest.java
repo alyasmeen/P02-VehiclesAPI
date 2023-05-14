1340f0ad4d5540d21b594db1234c629e59167ab4
Alyasmeen Abdulaziz Alzahrani <alyalzahrani@elm.com.sa>
2023-05-14 12:17:05 +0300

    first commit

---
+++ eureka/src/test/java/com/udacity/eureka/EurekaTest.java
@@ -0,0 +1,17 @@
+package com.udacity.eureka;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest(classes=EurekaTest.class)
+public class EurekaTest {
+
+    @Test
+    public void contextLoads() {
+    }
+
+
+}
